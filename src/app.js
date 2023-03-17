4ed8715b45395738dd02fe4b29fc5d5912680ada
Balim1 <jpmontoya70@gmail.com>
2023-03-17 23:39:08 +0000

    comment

--- src/app.js
+++ src/app.js
@@ -1,4 +1,22 @@
-window.onload = function() {
-  //write your code here
-  console.log("hola");
-};
+function dNameGenerator() {
+  const pronoun = ["the", "your"];
+
+  const adj = ["grand", "big"];
+
+  const noun = [".com", ".net"];
+
+  let generator = [];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let m = 0; m < adj.length; m++) {
+      for (let n = 0; n < noun.length; n++) {
+        let domain = pronoun[i] + adj[m] + noun[n];
+        generator.push(domain);
+      }
+    }
+  }
+  return generator;
+}
+document.querySelector(".domain-name").innerHTML = dNameGenerator().join(
+  "<br/>"
+);
